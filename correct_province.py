223e72855f2b412e1eafbda47cdada4d78d14445
Virut Ratinimittham <virut.r@7solutions.co.th>
2024-03-11 17:25:34 +0700

    initiatep project

---
+++ correct_province.py
@@ -0,0 +1,101 @@
+from fuzzywuzzy import process
+
+
+# List of correct province names
+corrected_provinces = [
+    "กระบี่",
+    "กรุงเทพมหานคร",
+    "กาญจนบุรี",
+    "กาฬสินธุ์",
+    "กำแพงเพชร",
+    "ขอนแก่น",
+    "จันทบุรี",
+    "ฉะเชิงเทรา",
+    "ชลบุรี",
+    "ชัยนาท",
+    "ชัยภูมิ",
+    "ชุมพร",
+    "เชียงราย",
+    "เชียงใหม่",
+    "ตรัง",
+    "ตราด",
+    "ตาก",
+    "นครนายก",
+    "นครปฐม",
+    "นครพนม",
+    "นครราชสีมา",
+    "นครศรีธรรมราช",
+    "นครสวรรค์",
+    "นนทบุรี",
+    "นราธิวาส",
+    "น่าน",
+    "บึงกาฬ",
+    "บุรีรัมย์",
+    "ปทุมธานี",
+    "ประจวบคีรีขันธ์",
+    "ปราจีนบุรี",
+    "ปัตตานี",
+    "พระนครศรีอยุธยา",
+    "พะเยา",
+    "พังงา",
+    "พัทลุง",
+    "พิจิตร",
+    "พิษณุโลก",
+    "เพชรบุรี",
+    "เพชรบูรณ์",
+    "แพร่",
+    "ภูเก็ต",
+    "มหาสารคาม",
+    "มุกดาหาร",
+    "แม่ฮ่องสอน",
+    "ยโสธร",
+    "ยะลา",
+    "ร้อยเอ็ด",
+    "ระนอง",
+    "ระยอง",
+    "ราชบุรี",
+    "ลพบุรี",
+    "ลำปาง",
+    "ลำพูน",
+    "เลย",
+    "ศรีสะเกษ",
+    "สกลนคร",
+    "สงขลา",
+    "สตูล",
+    "สมุทรปราการ",
+    "สมุทรสงคราม",
+    "สมุทรสาคร",
+    "สระแก้ว",
+    "สระบุรี",
+    "สิงห์บุรี",
+    "สุโขทัย",
+    "สุพรรณบุรี",
+    "สุราษฎร์ธานี",
+    "สุรินทร์",
+    "หนองคาย",
+    "หนองบัวลำภู",
+    "อ่างทอง",
+    "อำนาจเจริญ",
+    "อุดรธานี",
+    "อุทัยธานี",
+    "อุตรดิตถ์",
+    "อุบลราชธานี",
+    "อ่างทอง"
+]
+
+# Function to correct province name
+def correct_province(province):
+    if province in corrected_provinces:
+        return province
+    else:
+        # Use fuzzy string matching to find the closest match
+        closest_match, score = process.extractOne(province, corrected_provinces)
+        if score >= 80:  # Adjust the threshold as needed
+            return closest_match
+        else:
+            return province  # Return the original province name if not a close match
+
+# Example usage
+misspelled_province = "เขียงใหม่"
+corrected_province = correct_province(misspelled_province)
+print(corrected_province)
